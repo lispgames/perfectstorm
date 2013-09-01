c3ee775590133498cd3abb92297d639f839d3f16
gpadd <unknown.areth@gmail.com>
2013-09-01 19:50:02 +0200

    Added function: get-surface-data-as-array which was in the old cl-cairo

--- perfectstorm/util.lisp
+++ perfectstorm/util.lisp
@@ -1,6 +1,6 @@
 (in-package :storm)
 
-; sybs dirty little secrets 
+; sybs dirty little secrets
 
 (defclass range ()
   ((left-bound
@@ -18,12 +18,12 @@
 
 (defmethod in-range-p ((x number) (r range))
   "Checks if the range r contains x."
-  (with-slots 
+  (with-slots
    (left-bound open-to-left right-bound open-to-right) r
    (and
     (if open-to-left  (> x left-bound) (>= x left-bound))
     (if open-to-right (< x right-bound) (<= x right-bound)))))
-  
+
 
 ; (in-range-p x #R{a b})
 (set-dispatch-macro-character #\# #\R
@@ -34,8 +34,17 @@
 	   (left-bound (first inner-expression))
 	   (right-bound (second inner-expression))
 	   (open-to-right (char= #\} (read-char stream)))) ; TODO error handling
-      `(make-instance 'range 
+      `(make-instance 'range
 		      :left-bound ,left-bound
 		      :open-to-left ,open-to-left
 		      :right-bound ,right-bound
 		      :open-to-right ,open-to-right))))
+
+(defun get-surface-data-as-array (surface bytes-per-pixel)
+  (let* ((width (cairo:image-surface-get-width surface))
+         (height (cairo:image-surface-get-height surface))
+         (buffer (make-array (* width height bytes-per-pixel) :element-type '(unsigned-byte 8) :fill-pointer 0))
+         (data (cairo:image-surface-get-data surface)))
+    (loop for i from 0 below (* width height bytes-per-pixel) do
+         (vector-push-extend (cffi:mem-ref data :uint8 i) buffer))
+    buffer))
