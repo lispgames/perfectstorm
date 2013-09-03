b405ae0866c5560ab78e8ee0f3cb5970dd36a1d4
gpadd <unknown.areth@gmail.com>
2013-09-03 20:19:19 +0200

    Removing file.

--- load-perfectstorm.lisp
+++ load-perfectstorm.lisp
@@ -1,10 +1,1 @@
-;;; Run this as a script to load perfectstorm and the libraries it uses.
 
-
-(push "./vektor/" asdf:*central-registry*)
-(push "./toolbox/" asdf:*central-registry*)
-(push "./perfectstorm/" asdf:*central-registry*)
-
-(asdf:load-system :vektor)
-(asdf:load-system :toolbox)
-(asdf:load-system :perfectstorm)
