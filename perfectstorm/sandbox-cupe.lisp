bd37e4a401ec1a38dece72829c8717a21cba580f
gpadd <unknown.areth@gmail.com>
2013-10-22 18:08:28 +0200

    Removed a lot of whitespaces.

--- perfectstorm/sandbox-cupe.lisp
+++ perfectstorm/sandbox-cupe.lisp
@@ -1,33 +1,33 @@
 (in-package :storm)
 
-(defclass static-class (sb-mop:standard-class)
+(defclass static-class (c2mop:standard-class)
   ((static-slots
     :initform ()
     :accessor static-slots)
    (output
     :accessor output)))
 
-(defclass static-direct-slot-definition (sb-mop:standard-direct-slot-definition)
+(defclass static-direct-slot-definition (c2mop:standard-direct-slot-definition)
   ((static-p
     :initform nil
     :accessor static-p
     :initarg :static)))
 
-(defclass static-effective-slot-definition (sb-mop:standard-effective-slot-definition)
+(defclass static-effective-slot-definition (c2mop:standard-effective-slot-definition)
   ((static-p
     :initform nil
     :accessor static-p
     :initarg :static)))
 
-(defmethod sb-mop:direct-slot-definition-class ((class static-class) &key &allow-other-keys)
+(defmethod c2mop:direct-slot-definition-class ((class static-class) &key &allow-other-keys)
   (find-class 'static-direct-slot-definition))
 
-(defmethod sb-mop:effective-slot-definition-class ((class static-class) &key &allow-other-keys)
+(defmethod c2mop:effective-slot-definition-class ((class static-class) &key &allow-other-keys)
   (find-class 'static-effective-slot-definition))
 
 
 (defparameter *output* "")
-(defmethod sb-mop:compute-effective-slot-definition ((class static-class) slot-name direct-slot-definitions)
+(defmethod c2mop:compute-effective-slot-definition ((class static-class) slot-name direct-slot-definitions)
   (declare (ignore slot-name))
   (let ((effective-slotdef (call-next-method))) ;let CLOS do the lifting
     (setf (output class) (format nil "~a" effective-slotdef)); direct-slot-definitions)
@@ -35,7 +35,7 @@
           (static-p (first direct-slot-definitions))) ;;wtf?
     effective-slotdef))
 
-(defmethod sb-mop:validate-superclass ((class static-class) super)
+(defmethod c2mop:validate-superclass ((class static-class) super)
   t)
 
 (defclass foo ()
@@ -48,4 +48,3 @@
 ;
 ;(slot-value (make-instance 'foo) 'bar)
 ;(static-slot-value 'foo 'bar)
-
